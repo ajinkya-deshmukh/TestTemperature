04ac56a73bd9fafc29df183bb0db7cee703b0ea6
Ajinkya Deshmukh <ajinkyadeshmukh@AjinkyaD-InteliMac.local>
2021-01-10 12:31:46 +0530

    First Commit: Project Base

---
+++ src/main/java/ObjectRepository/NDTVHome.java
@@ -0,0 +1,10 @@
+package ObjectRepository;
+
+public class NDTVHome {
+	
+	public static String link_latest = "//a[contains(text(),'LATEST')]";
+	public static String btn_section = "//div[@id='topnav_section']";
+	public static String link_weather = "//li[@class='n_weather']//a";
+	public static String btn_accept_alert = "//a[@class='notnow']";
+
+}
---
+++ src/main/java/ObjectRepository/WeatherMap.java
@@ -0,0 +1,12 @@
+package ObjectRepository;
+
+public class WeatherMap {
+	public static String img_weather_logo = "//div[@id='logo']/img";
+	public static String lbl_pin_city = "//span[contains(text(),'Pin your City')]";
+	public static String  txt_search_city = "//input[@id='searchBox']";
+
+	// Parameterized elements based on city name given in .env
+	//public static String  chk_search_city = f"//input[@id='{config.city_name}' and @type='checkbox']";
+	//public static String  lbl_degree_temp_on_map = f"//div[@title='{config.city_name}']//span[@class='tempRedText']";
+
+}
